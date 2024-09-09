940271edbbbf420d7ae49735e61a43d10f1f6d66
jam089 <jam089@mail.ru>
2024-09-10 02:05:23 +0300

    include core.config.settings

--- app/main.py
+++ app/main.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI
 import uvicorn
 
+from core.config import settings
+
 app = FastAPI()
 
 if __name__ == "__main__":
     uvicorn.run(
         "main:app",
-        host="0.0.0.0",
-        port=8000,
+        host=settings.run.host,
+        port=settings.run.port,
+        reload=settings.run.reload,
     )
