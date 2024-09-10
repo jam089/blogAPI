aa0e036027481ee7551474e2fcb39c63318a054e
jam089 <jam089@mail.ru>
2024-09-10 16:17:12 +0300

    add missing str()

--- app/core/db_helper.py
+++ app/core/db_helper.py
@@ -42,7 +42,7 @@ class DatabaseHelper:
 
 
 db_helper = DatabaseHelper(
-    url=settings.db.url,
+    url=str(settings.db.url),
     echo=settings.db.echo,
     echo_pool=settings.db.echo_pool,
     pool_size=settings.db.pool_size,
