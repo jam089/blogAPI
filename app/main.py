de80353943d605d977c2bac19acab75758235bc8
jam089 <jam089@mail.ru>
2024-09-11 21:51:53 +0300

    include general api router in app

--- app/main.py
+++ app/main.py
@@ -2,8 +2,11 @@ from fastapi import FastAPI
 import uvicorn
 
 from core import settings
+from api import router as api_router
 
 app = FastAPI()
+
+app.include_router(api_router)
 
 if __name__ == "__main__":
     uvicorn.run(
