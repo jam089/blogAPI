c69a4e7af5344298d8221111b0f4bd79a24a1fd0
jam089 <jam089@mail.ru>
2024-09-11 21:54:26 +0300

    replace from fix article schem params to config settings params

--- app/api/schemes/article.py
+++ app/api/schemes/article.py
@@ -2,23 +2,44 @@ from datetime import datetime
 
 from pydantic import BaseModel, ConfigDict, Field
 
+from core import settings
+
 
 class BaseArticleSchm(BaseModel):
     pass
 
 
 class CreateArticleSchm(BaseArticleSchm):
-    title: str = Field(max_length=120)
-    text: str = Field(min_length=260)
-    topic: str = Field(max_length=36)
-    author_name: str = Field(max_length=50)
+    title: str = Field(
+        min_length=settings.article_param.title_min_length,
+        max_length=settings.article_param.title_max_length,
+    )
+    text: str = Field(min_length=settings.article_param.text_min_length)
+    topic: str = Field(
+        min_length=settings.article_param.topic_min_length,
+        max_length=settings.article_param.topic_max_length,
+    )
+    author_name: str = Field(max_length=settings.article_param.author_name_max_length)
 
 
 class ChangeArticleSchm(BaseArticleSchm):
-    title: str | None = Field(None, max_length=120)
-    text: str | None = Field(None, min_length=260)
-    topic: str | None = Field(None, max_length=36)
-    author_name: str | None = Field(None, max_length=50)
+    title: str | None = Field(
+        None,
+        min_length=settings.article_param.title_min_length,
+        max_length=settings.article_param.title_max_length,
+    )
+    text: str | None = Field(
+        None,
+        min_length=settings.article_param.text_min_length,
+    )
+    topic: str | None = Field(
+        None,
+        min_length=settings.article_param.topic_min_length,
+        max_length=settings.article_param.topic_max_length,
+    )
+    author_name: str | None = Field(
+        None, max_length=settings.article_param.author_name_max_length
+    )
 
 
 class ReadArticleSchm(CreateArticleSchm):
