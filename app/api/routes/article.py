927dd084a43ea218a87a98fb504bc45d5ab0bbb9
jam089 <jam089@mail.ru>
2024-09-12 00:45:55 +0300

    add get_article endpoint

--- app/api/routes/article.py
+++ app/api/routes/article.py
@@ -1,15 +1,30 @@
 from typing import Annotated, Sequence
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from api import crud, schemes
+from api import crud
+from api.schemes import ReadArticleSchm, ReadArticleWithCommentsSchm
 from core import db_helper
 
 router = APIRouter()
 
 
-@router.get("/", response_model=Sequence[schemes.ReadArticleSchm])
+@router.get("/{article_id}/", response_model=ReadArticleWithCommentsSchm)
+async def get_article(
+    sess: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+    article_id: int,
+):
+    if result := await crud.get_article_with_five_last_comments(sess, article_id):
+        return result
+
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="article not found",
+    )
+
+
+@router.get("/", response_model=Sequence[ReadArticleSchm])
 async def get_all_articles(
     sess: Annotated[AsyncSession, Depends(db_helper.session_getter)],
 ):
