6c63f6e33013df966bed27ab0be5e12ff0552680
jam089 <jam089@mail.ru>
2024-09-12 20:15:19 +0300

    replace from fix comment scheme params to config settings params

--- app/api/schemes/comment.py
+++ app/api/schemes/comment.py
@@ -2,21 +2,33 @@ from datetime import datetime
 
 from pydantic import BaseModel, ConfigDict, Field
 
+from core import settings
+
 
 class BaseCommentSchm(BaseModel):
     pass
 
 
 class CreateCommentSchm(BaseCommentSchm):
-    comment_text: str = Field(max_length=260)
-    author_name: str = Field(max_length=50)
+    comment_text: str = Field(
+        min_length=settings.comment_param.comment_text_min_length,
+        max_length=settings.comment_param.comment_text_max_length,
+    )
+    author_name: str = Field(max_length=settings.comment_param.author_name_max_length)
     score: int = Field(ge=1, le=10)
     article_id: int
 
 
 class ChangeCommentSchm(BaseCommentSchm):
-    comment_text: str | None = Field(None, max_length=260)
-    author_name: str | None = Field(None, max_length=50)
+    comment_text: str | None = Field(
+        None,
+        min_length=settings.comment_param.comment_text_min_length,
+        max_length=settings.comment_param.comment_text_max_length,
+    )
+    author_name: str | None = Field(
+        None,
+        max_length=settings.comment_param.author_name_max_length,
+    )
     score: int | None = Field(None, ge=1, le=10)
 
 
