506a1e6b5de64a5aac6264e4b96e42c95d3af8e1
jam089 <jam089@mail.ru>
2024-09-12 21:24:48 +0300

    add get_comments_of_article endpoint

--- app/api/routes/comment.py
+++ app/api/routes/comment.py
@@ -9,13 +9,29 @@ from core import db_helper
 
 router = APIRouter()
 
-HTTP_404 = HTTPException(
+HTTP_404_comment = HTTPException(
     status_code=status.HTTP_404_NOT_FOUND,
     detail="comment not found",
 )
 
+HTTP_404_article = HTTPException(
+    status_code=status.HTTP_404_NOT_FOUND,
+    detail="article not found",
+)
 
-@router.get("/{comment_id}", response_model=ReadCommentSchm)
+
+@router.get("/", response_model=Sequence[ReadCommentSchm])
+async def get_comments_of_article(
+    sess: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+    article_id: int,
+):
+    if article := await crud.get_article(sess, article_id):
+        return await crud.get_comments_of_article(sess, article)
+
+    raise HTTP_404_article
+
+
+@router.get("/{comment_id}/", response_model=ReadCommentSchm)
 async def get_comment(
     sess: Annotated[AsyncSession, Depends(db_helper.session_getter)],
     comment_id: int,
@@ -23,4 +39,4 @@ async def get_comment(
     if comment := await crud.get_comment(sess, comment_id):
         return comment
 
-    raise HTTP_404
+    raise HTTP_404_comment
