bb71143ec8fb303df94f3dfefe53ae4e19043cc4
jam089 <jam089@mail.ru>
2024-09-13 19:39:52 +0300

    include admin router to api general router

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 
 from .routes.article import router as articles_router
 from .routes.comment import router as comments_router
+from .routes.administration import router as admin_router
 
 from core import settings
 
@@ -17,4 +18,9 @@ router.include_router(
     router=comments_router,
     prefix=settings.api.comments.prefix,
     tags=[settings.api.comments.tag],
+)
+router.include_router(
+    router=admin_router,
+    prefix=settings.api.admin.prefix,
+    tags=[settings.api.admin.tag],
 )
