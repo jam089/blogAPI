41324c8ebcbafc6f65ecc05bc41e9257834629b8
jam089 <jam089@mail.ru>
2024-10-11 04:37:40 +0300

    add index_articles endpoint

--- app/api/routes/administration.py
+++ app/api/routes/administration.py
@@ -1,5 +1,6 @@
-from typing import Annotated
+from typing import Annotated, List
 
+from elasticsearch import AsyncElasticsearch
 from fastapi import APIRouter, HTTPException, status
 from fastapi.params import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -7,6 +8,9 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from api import crud
 from core import db_helper, settings
 from core.utils.file_utils import json_read
+from core.models import Article
+from api.schemes import CreateArticleSchm
+from services.elasticsearch import index_docs, es
 
 router = APIRouter()
 
@@ -30,3 +34,17 @@ async def import_data_from_file(
             status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
             detail="can not process import data",
         )
+
+
+@router.get("/es_index_articles/")
+async def index_articles(
+    db_sess: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+    es_sess: Annotated[AsyncElasticsearch, Depends(es.es_getter)],
+) -> dict[str, int | List[int]]:
+    return await index_docs(
+        db_session=db_sess,
+        es_session=es_sess,
+        index_name=settings.es.articles_index,
+        sql_model=Article,
+        pydantic_schm=CreateArticleSchm,
+    )
