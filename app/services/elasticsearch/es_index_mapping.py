cb9367d6c27f6844a37b4e3d4e3189cefc1b09eb
jam089 <jam089@mail.ru>
2024-10-11 04:32:13 +0300

    create es_index_mapping.py for index mapping to create index (with article map)

---
+++ app/services/elasticsearch/es_index_mapping.py
@@ -0,0 +1,12 @@
+from core import settings
+
+
+article = {
+    "title": {"type": "text", "analyzer": "russian"},
+    "text": {"type": "text", "analyzer": "russian"},
+    "topic": {"type": "text", "analyzer": "russian"},
+    "author_name": {"type": "text", "analyzer": "russian"},
+}
+
+
+index_dict = {settings.es.articles_index: article}
