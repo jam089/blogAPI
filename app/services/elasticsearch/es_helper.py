a5f36d9a5370ff23cc9277ce482ce1a2411e047b
jam089 <jam089@mail.ru>
2024-10-11 04:31:07 +0300

    create helper for elasticsearch

---
+++ app/services/elasticsearch/es_helper.py
@@ -0,0 +1,30 @@
+from contextlib import asynccontextmanager
+from typing import AsyncGenerator
+
+from core import settings
+
+from elasticsearch import AsyncElasticsearch
+
+
+class ESHelper:
+    def __init__(
+        self,
+        url_list: list[str],
+    ):
+        self.url_list = url_list
+
+    @asynccontextmanager
+    async def es_client(self) -> AsyncElasticsearch:
+        es_client = AsyncElasticsearch(hosts=self.url_list)
+        yield es_client
+        await es_client.close()
+
+    async def es_getter(self) -> AsyncGenerator[AsyncElasticsearch, None]:
+        async with self.es_client() as es_sess:
+            yield es_sess
+            await es_sess.close()
+
+
+es = ESHelper(
+    url_list=[str(settings.es.url)],
+)
