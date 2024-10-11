22c682ef895faf5045de0fc6e2af9d0f14c779a7
jam089 <jam089@mail.ru>
2024-10-11 04:36:27 +0300

    add lifespan func with checking elasticsearch index exist

--- app/main.py
+++ app/main.py
@@ -1,10 +1,20 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 import uvicorn
 
 from core import settings
 from api import router as api_router
+from services.elasticsearch import check_index as es_check_idx
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await es_check_idx(index_name=settings.es.articles_index)
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(api_router)
 
