2e7ba51136d443c0b54aae088b6841630a9131dc
Ivan Filippov <jam089@mail.ru>
2025-10-02 12:05:09 +0300

    apply sync redis client getter, add else-block to if isinstance

--- app/services/redis/redis_utils.py
+++ app/services/redis/redis_utils.py
@@ -27,20 +27,22 @@ def redis_cache(
     def decorator(func: Callable[P, Awaitable[R]]) -> Callable[P, Awaitable[R]]:
         @wraps(func)
         async def wrapper(*args: P.args, **kwargs: P.kwargs) -> R:
-            async with r_cache.redis_client() as cache:
-                func_name = func.__name__
-                type_adapter = TypeAdapter(model_type)
+            cache = r_cache.redis_client()
+            func_name = func.__name__
+            type_adapter = TypeAdapter(model_type)
 
-                if cached_response := await cache.get(func_name):
-                    decoded_response: R = type_adapter.validate_json(cached_response)
-                    return decoded_response
+            if cached_response := await cache.get(func_name):
+                decoded_response: R = type_adapter.validate_json(cached_response)
+                return decoded_response
 
-                response = await func(*args, **kwargs)
-                if isinstance(response, Sequence) and not isinstance(response, str):
-                    sub_type: Any = get_args(model_type)[0]
-                    schm = [sub_type.model_validate(db_model) for db_model in response]
+            response = await func(*args, **kwargs)
+            if isinstance(response, Sequence) and not isinstance(response, str):
+                sub_type: Any = get_args(model_type)[0]
+                schm = [sub_type.model_validate(db_model) for db_model in response]
                 encoded = type_adapter.dump_json(schm).decode("utf-8")
-                await cache.set(func_name, encoded, ex=expire)
+            else:
+                encoded = type_adapter.dump_json(response).decode("utf-8")
+            await cache.set(func_name, encoded, ex=expire)
             return response
 
         if inactive:
