0b9a9a47cd39dc2d7da622aaff26835f6129cf64
Ivan Filippov <jam089@mail.ru>
2025-10-03 20:08:57 +0300

    tests for article hybrid properties

---
+++ tests/unit/test_models_article.py
@@ -0,0 +1,38 @@
+import pytest
+from core.models import Article, Comment
+
+
+@pytest.mark.asyncio
+async def test_article_score_property_non_comment() -> None:
+    article = Article(title="test_title", text="test_text")
+    article.comments = []
+    assert article.score == 0
+
+
+@pytest.mark.asyncio
+async def test_article_score_property_with_comments() -> None:
+    article = Article(title="test_title", text="test_text", id=1)
+    comment_1 = Comment(comment_text="test_text", score=2, article_id=1)
+    comment_2 = Comment(comment_text="test_text", score=4, article_id=1)
+    comment_3 = Comment(comment_text="test_text", score=6, article_id=1)
+    comment_4 = Comment(comment_text="test_text", score=8, article_id=1)
+    article.comments = [comment_1, comment_2, comment_3, comment_4]
+    assert article.score == 5
+
+
+@pytest.mark.asyncio
+async def test_article_absolute_score_property_non_comment() -> None:
+    article = Article(title="test_title", text="test_text")
+    article.comments = []
+    assert article.absolut_score == 0
+
+
+@pytest.mark.asyncio
+async def test_article_absolute_score_property_with_comments() -> None:
+    article = Article(title="test_title", text="test_text", id=1)
+    comment_1 = Comment(comment_text="test_text", score=2, article_id=1)
+    comment_2 = Comment(comment_text="test_text", score=4, article_id=1)
+    comment_3 = Comment(comment_text="test_text", score=6, article_id=1)
+    comment_4 = Comment(comment_text="test_text", score=8, article_id=1)
+    article.comments = [comment_1, comment_2, comment_3, comment_4]
+    assert article.absolut_score == 20
