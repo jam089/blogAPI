2b973f4977ac26ce4a6f79bc7dba3a263fcde320
Ivan Filippov <jam089@mail.ru>
2025-10-04 11:29:49 +0300

    tests redis help

---
+++ tests/unit/test_redis_helper.py
@@ -0,0 +1,22 @@
+from services.redis.redis_helper import RedisHelper
+
+
+def test_single_client() -> None:
+    redis_helper = RedisHelper(
+        url="redis://",
+        encoding="utf-8",
+        decode_responses=True,
+    )
+    expected_pool = redis_helper.pool
+    client_1 = redis_helper.redis_client()
+    client_2 = redis_helper.redis_client()
+    expected_client = redis_helper._client
+    client_1_pool = client_1.connection_pool
+    client_2_pool = client_2.connection_pool
+
+    assert client_1_pool == expected_pool
+    assert client_2_pool == expected_pool
+    assert client_2_pool == client_1_pool
+    assert client_1 == expected_client
+    assert client_2 == expected_client
+    assert client_2 == client_1
