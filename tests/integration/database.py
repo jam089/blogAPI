77171c108236b816887119876ba8e5d91b862eec
Ivan Filippov <jam089@mail.ru>
2025-10-08 20:47:05 +0300

    rewrite to fixtures style, correct SQLs, add test_db_url fixture

--- tests/integration/database.py
+++ tests/integration/database.py
@@ -1,86 +1,134 @@
+import os
 from collections.abc import AsyncGenerator
+from typing import Any
 
 import pytest
+import pytest_asyncio
 from core import db_helper, settings
 from core.models import Base
 from httpx import ASGITransport, AsyncClient
 from main import app
 from sqlalchemy import NullPool, text
-from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
-
-test_engine = create_async_engine(
-    url=str(settings.db.url),
-    poolclass=NullPool,
-)
-test_session_factory = async_sessionmaker(
-    bind=test_engine,
-    autoflush=False,
-    autocommit=False,
-    expire_on_commit=False,
+from sqlalchemy.ext.asyncio import (
+    AsyncEngine,
+    AsyncSession,
+    async_sessionmaker,
+    create_async_engine,
 )
 
 
-async def override_dispose() -> None:
-    await test_engine.dispose()
+@pytest.fixture(scope="package", autouse=True)
+def test_db_url(pg_service: dict[str, Any]) -> str:
+    user = os.environ["BLOGAPI__DB__LOGIN"]
+    password = os.environ["BLOGAPI__DB__PASS"]
+    db_name = os.environ["BLOGAPI__DB__SCHEMA"]
+    return f"postgresql+asyncpg://{user}:{password}@{pg_service['host']}:{pg_service['port']}/{db_name}"
 
 
-async def override_session_getter() -> AsyncGenerator[AsyncSession, None]:
-    async with test_session_factory() as session:
-        yield session
+@pytest.fixture(scope="package")
+def create_test_engine(test_db_url: str) -> AsyncEngine:
+    test_engine = create_async_engine(
+        url=test_db_url,
+        poolclass=NullPool,
+    )
+    return test_engine
 
 
-app.dependency_overrides[db_helper.session_getter] = override_session_getter
-app.dependency_overrides[db_helper.dispose] = override_dispose
+@pytest.fixture(scope="package")
+def create_test_session_factory(
+    create_test_engine: AsyncEngine,
+) -> async_sessionmaker[AsyncSession]:
+    test_session_factory = async_sessionmaker(
+        bind=create_test_engine,
+        autoflush=False,
+        autocommit=False,
+        expire_on_commit=False,
+    )
+    return test_session_factory
 
 
-@pytest.fixture(autouse=True)
-async def prepare_db() -> AsyncGenerator[None, None]:
-    async with test_engine.begin() as conn:
+@pytest_asyncio.fixture(scope="function", autouse=True)
+async def override_dependency(
+    create_test_engine: AsyncEngine,
+    create_test_session_factory: async_sessionmaker[AsyncSession],
+) -> AsyncGenerator[None, None]:
+    async def override_dispose() -> None:
+        await create_test_engine.dispose()
+
+    async def override_session_getter() -> AsyncGenerator[AsyncSession, None]:
+        async with create_test_session_factory() as session:
+            yield session
+
+    app.dependency_overrides[db_helper.session_getter] = override_session_getter
+    app.dependency_overrides[db_helper.dispose] = override_dispose
+
+    yield
+
+
+@pytest_asyncio.fixture(scope="function", autouse=True)
+async def prepare_db(
+    create_test_engine: AsyncEngine,
+    create_test_session_factory: async_sessionmaker[AsyncSession],
+) -> AsyncGenerator[None, None]:
+    async with create_test_engine.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
-    async with test_engine.begin() as conn:
+    async with create_test_engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
-    async with test_session_factory() as conn:
+    async with create_test_session_factory() as conn:
         await conn.begin()
         await conn.execute(
             text(
                 """
-            CREATE OR REPLACE FUNCTION update_last_update_column()
-            RETURNS TRIGGER AS $$
-            BEGIN
-                NEW.last_update_at = NOW();
-                RETURN NEW;
-            END;
-            $$ LANGUAGE plpgsql;
-            """
+                CREATE OR REPLACE FUNCTION update_last_updated_column()
+                RETURNS TRIGGER AS $$
+                BEGIN
+                    NEW.last_updated_at = NOW();
+                    RETURN NEW;
+                END;
+                $$ LANGUAGE plpgsql;
+                """
             )
         )
         await conn.commit()
         await conn.execute(
             text(
                 """
-            CREATE TRIGGER last_update_trigger
-            BEFORE UPDATE ON users
-            FOR EACH ROW
-            EXECUTE FUNCTION update_last_update_column();
-            """
+                CREATE TRIGGER last_updated_trigger
+                BEFORE UPDATE ON articles
+                FOR EACH ROW
+                EXECUTE FUNCTION update_last_updated_column();
+                """
+            )
+        )
+        await conn.commit()
+        await conn.execute(
+            text(
+                """
+                CREATE TRIGGER last_updated_trigger
+                BEFORE UPDATE ON comments
+                FOR EACH ROW
+                EXECUTE FUNCTION update_last_updated_column();
+                """
             )
         )
         await conn.commit()
 
     yield
 
-    async with test_engine.begin() as conn:
+    async with create_test_engine.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
 
 
-@pytest.fixture
-async def test_session() -> AsyncGenerator[AsyncSession, None]:
-    async with test_session_factory() as session:
+@pytest_asyncio.fixture(scope="function")
+async def test_session(
+    create_test_session_factory: async_sessionmaker[AsyncSession],
+) -> AsyncGenerator[AsyncSession, None]:
+    async with create_test_session_factory() as session:
         yield session
 
 
-@pytest.fixture
-async def async_client() -> AsyncGenerator[AsyncClient, None]:
+@pytest_asyncio.fixture(scope="function")
+async def async_client(app_service: str) -> AsyncGenerator[AsyncClient, None]:
     async with AsyncClient(
         transport=ASGITransport(app),
         base_url=f"http://test{settings.api.prefix}",
