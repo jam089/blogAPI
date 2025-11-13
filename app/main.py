513bf196b94032600fdbe6d093564ff353379aba
Ivan Filippov <jam089@mail.ru>
2025-11-13 12:21:18 +0300

    cors

--- app/main.py
+++ app/main.py
@@ -28,6 +28,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:8000",
+        "http://blogapp.spacejamhd.com",
         "https://blogapp.spacejamhd.com",
         "*",
     ],
